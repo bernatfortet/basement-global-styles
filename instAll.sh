13039bc4eae81089bea5306eab4a3bda305c2a3f
Bernat <bernatfortet@gmail.com>
2018-08-07 11:21:46 -0700

    Change repo

--- instAll.sh
+++
@@ -1,9 +0,0 @@
-cd ./global-styles && npm install
-cd ..
-cd ./react-components && npm install
-cd ..
-cd ./redux-entities 	&& npm install
-cd ..
-cd ./redux-firebase 	&& npm install
-cd ..
-cd ./utils 						&& npm install
--- publishAll.sh
+++
@@ -1,9 +0,0 @@
-cd ./global-styles && npm publish --access public
-cd ..
-cd ./react-components && npm publish --access public
-cd ..
-cd ./redux-entities 	&& npm publish --access public
-cd ..
-cd ./redux-firebase 	&& npm publish --access public
-cd ..
-cd ./utils 						&& npm publish --access public
