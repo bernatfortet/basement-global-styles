012a368ae9bb13840adbd8cd68235e8f1fa6b94e
Bernat <bernatfortet@gmail.com>
2019-01-05 12:48:08 -0800

    Untrack dist folder

--- dist/primitive.d.ts
+++
@@ -1,8 +0,0 @@
-import { SpacingProps } from './types';
-export declare const dimensionProps: any;
-export declare const spacingProps: (props: SpacingProps) => any[];
-export declare const flexProps: any;
-export declare const positionProps: any;
-export declare const appearanceProps: any;
-export declare const textProps: any;
-export declare const boxProps: any;
--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const is: (n: any) => boolean;
-export declare const isNum: (n: any) => boolean;
-export declare const parseUnit: (n: string | number) => string | number;
-export declare const isNegative: (n: number) => boolean;
-export declare const createMediaQuery: (n: string | number) => string;
-export declare const defaultBreakpoints: number[];
